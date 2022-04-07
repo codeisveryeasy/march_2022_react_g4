93b99e9c5b1aa77ae10b9c821202c413ba2bed52
codeisveryeasy <89445533+codeisveryeasy@users.noreply.github.com>
2022-04-07 18:29:32 +0530

    Add files via upload

--- src/spa/pages/contact.js
+++ src/spa/pages/contact.js
@@ -15,21 +15,40 @@ class Contact extends React.Component {
         //console.log(this.state.name)
     }
 
+    captureEmail=(event)=>{
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    resetCaptures=()=>{
+        this.setState({
+            name:"",
+            email:""
+        })
+    }
+
     render() { 
         return (
             <div>
                 <h1>Contact</h1>
                 Name: 
-                <input onChange={this.captureName} 
+                <input 
+                        onChange={this.captureName} 
                         value={this.state.name}
                 ></input> 
                 {this.state.name}
                 <br></br><br></br>
                 Email: 
-                <input></input> 
+                <input
+                        onChange={this.captureEmail}
+                        value={this.state.email}
+                ></input> 
+                {this.state.email}
                 <br></br><br></br>
                 <button>Send</button>
-                
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+                <button onClick={this.resetCaptures}>Reset</button>
             </div>
         );
     }
